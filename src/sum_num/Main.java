8494f5cd4d4c70b6d54ced928488736737146e7b
chohj2614 <hyunjoon0614@naver.com>
2025-11-17 16:20:26 +0900

    sum_num passed

--- src/sum_num/Main.java
+++ src/sum_num/Main.java
@@ -9,11 +9,13 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int len = Integer.parseInt(br.readLine());
         String temp = br.readLine();
-        String[] num = temp.toCharArray();
+        char[] num = temp.toCharArray();
+        int sum = 0;
         for(int i = 0; i < len; i++){
-            int cur = Integer.parseInt(num.charAt(i));
+            int cur = num[i] - '0';
+            sum = cur + sum;
         }
 
-        System.out.print(msg.length);
+        System.out.print(sum);
     }
 }
