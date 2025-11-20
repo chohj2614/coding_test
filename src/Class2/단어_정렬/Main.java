0af9f3207c36a90b93527072a1d1e9e58aa25b59
chohj2614 <hyunjoon0614@naver.com>
2025-11-20 17:13:02 +0900

    단어 정렬 통과

---
+++ src/Class2/단어_정렬/Main.java
@@ -0,0 +1,22 @@
+package Class2.단어_정렬;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        List<String> list = new ArrayList<>();
+        for(int i = 0; i < N; i++){
+            list.add(br.readLine());
+        }
+        list.sort(Comparator.comparingInt(String::length).thenComparing(String::compareTo));
+        List<String> fin = new ArrayList<>(new LinkedHashSet<>(list));
+        for(String s: fin){
+            System.out.println(s);
+        }
+    }
+}
