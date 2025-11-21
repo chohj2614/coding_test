8c6e389c3393d83a9c82e2e35b83cfc517938133
chohj2614 <hyunjoon0614@naver.com>
2025-11-21 15:03:40 +0900

    큐 통과

---
+++ src/Class2/큐/Main.java
@@ -0,0 +1,48 @@
+
+package Class2.큐;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        ArrayDeque<Integer> queue = new ArrayDeque<>();
+        for(int i = 0; i < N; i++){
+            String[] line = br.readLine().split(" ");
+            switch(line[0]){
+                case "push":
+                    queue.addLast(Integer.parseInt(line[1]));
+                    break;
+                case "pop":
+                    if(queue.isEmpty()){
+                        System.out.println(-1);
+                    } else{
+                        System.out.println(queue.poll());
+                    }
+                    break;
+                case "size":
+                    System.out.println(queue.size());
+                    break;
+                case "empty":
+                    if(queue.isEmpty()){
+                        System.out.println(1);
+                    }else {System.out.println(0);}
+                    break;
+                case "front":
+                    if(queue.isEmpty()){
+                        System.out.println(-1);
+                    }else {System.out.println(queue.peekFirst());}
+                    break;
+                case "back":
+                    if(queue.isEmpty()){
+                        System.out.println(-1);
+                    }else {System.out.println(queue.peekLast());}
+                    break;
+            }
+        }
+    }
+}
