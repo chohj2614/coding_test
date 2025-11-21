766902bd15540d8f91fac1c8593a27cab5100182
chohj2614 <hyunjoon0614@naver.com>
2025-11-21 17:52:11 +0900

    체스판 풀이중

--- src/Class2/체스판_다시_칠하기/Main.java
+++ src/Class2/체스판_다시_칠하기/Main.java
@@ -7,19 +7,48 @@ import java.nio.Buffer;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int[] board = Arrays.stream(br.readLine().split(" ")).map(Integer::parseInt).collect(Collectors.toList()).toArray();
-        int count = 0;
-        int min = 0;
-        String[] bw = {"W", "B"};
-        for(int i = 0; i < 2; i++){
-            String start = bw[i];
 
+    //보드 숫자 받기
+        List<Integer> board = Arrays.stream(br.readLine().split(" "))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+    //보드 맵 받기
+        List<String> bwMap = new ArrayList<>();
+        for(int i = 0; i < board.get(0); i++){
+            bwMap.add(br.readLine());
         }
+    //2번 bruteforce
+        int min = 0;
+
+        for(int i =0; i < 2; i++){
+            int count = 0;
+            char start = i==0?'B':'W';
+            char cur;
+            char prev;
+            for(int j = 0; j < board.get(0); j++){
+                if(j ==0){
+                    prev = start;
+                    cur = prev;
+                } else {
+                    cur = prev=="W"?"B":"W";
+                }
+                for(char c : bwMap.get(j).toCharArray()){
+                    if(c != cur){
+                        count++;
+                    }
+                }
+            }
+        }
+    //min 출력
+
+
+
 
     }
 }
