077b002c61adf784777e8bb4db5e337e1167a370
chohj2614 <101245494+chohj2614@users.noreply.github.com>
2025-11-23 15:59:18 +0900

    [Silver IV] Title: 괄호, Time: 104 ms, Memory: 14256 KB -BaekjoonHub

---
+++ Java/백준/Silver/9012. 괄호/괄호.java
@@ -0,0 +1,31 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        for(int i = 0; i < T; i++){
+            String cur = br.readLine();
+            int l = 0;
+            boolean yes = true;
+            for(char c : cur.toCharArray()){
+                if( c == '('){
+                    l++;
+                }
+                else if( c == ')' && l > 0){
+                    l--;
+                }else{
+                    yes = false;
+                }
+            }
+            if(yes && l == 0){
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+        }
+    }
+}
