830d1a42b7deda8ba6e24f181cf8507e31d7ad04
chohj2614 <hyunjoon0614@naver.com>
2025-11-25 17:49:28 +0900

    덩치 풀이중

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,24 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] str = br.readLine().split(" ");
+        StringBuilder sb = new StringBuilder();
 
-        int A = Integer.parseInt(str[0]);
-        int B = Integer.parseInt(str[1]);
-        int V = Integer.parseInt(str[2]);
-//        int count = 1;
-//        while(V>0){
-//            V -= A;
-//            if(V <= 0){
-//                break;
-//            }
-//            V += B;
-//            count++;
-//        } loop 쓰면 시간 초과
-        int count = (int)Math.ceil((V - A) / (double)(A-B))  + 1;
-                // V- A : 마지막 날 올라갔을 높이를 차감
-                // A-B 로서 마지막 날 까지 필요한 날짜
-                // 1 : 첫날도 포함이라 +1
-        System.out.println(count);
+        int N = Integer.parseInt(br.readLine());
+        List<List<Integer>> inputs = new ArrayList<>();
+
+        for(int i = 0; i< N; i++){
+            List<Integer> temp = new ArrayList<>();
+            String[] newLine = br.readLine().split(" ");
+            temp.add(Integer.parseInt(newLine[0]));
+            temp.add(Integer.parseInt(newLine[1]));
+            inputs.add(temp);
+        }
+
     }
 }
