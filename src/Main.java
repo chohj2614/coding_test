afa8d8a3404ebadaf035efce05d7f6f97353b4c0
chohj2614 <hyunjoon0614@naver.com>
2025-11-27 17:47:04 +0900

    스택 수열 풀이중

--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,43 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        // 의견수 저장
+        StringBuilder sb = new StringBuilder();
+
         int N = Integer.parseInt(br.readLine());
-//        List<Integer> list = new ArrayList<>();
-        int[] arr = new int[N];
-        // 의견들 저장
-        for(int i = 0; i < N; i++){
-//            list.add(Integer.parseInt(br.readLine()));
-            arr[i] = Integer.parseInt(br.readLine());
+
+        Stack<Integer> stack = new Stack<>();
+        Stack<Integer> toadd = new Stack<>();
+        for (int i = N; i > 0; i--) {
+            toadd.add(i);
         }
 
-        // 의견들 정렬
-//        Collections.sort(list);
-        Arrays.sort(arr);
-
-        // 극단값 자르기
-        int perc = (int)Math.round(N * 0.15);
-
-//        for(int i = 0; i< perc; i++){
-//            list.remove(0);
-//            list.remove(list.size() - 1 );
-//        }
-        int ans = 0;
-//        for(int x : list){
-//            ans += x;
-//        }
-        for(int i = perc; i < N - perc; i++){
-            ans += arr[i];
+        for (int i = 0; i < N; i++) {
+            int cur = Integer.parseInt(br.readLine());
+            if (stack.isEmpty()) {
+                stack.push(toadd.pop());
+                sb.append("+\n");
+            }
+            if(stack.peek() == cur){
+                stack.pop();
+                sb.append("-\n");
+            } else if (stack.peek() > cur){
+                while (stack.peek() != cur) {
+                    if (toadd.isEmpty()) {
+                        System.out.println("NO");
+                        return;
+                    }
+                    stack.push(toadd.pop());
+                    sb.append("+\n");
+                }
+                stack.pop();
+                sb.append("-\n");
+            }
         }
-//        ans = (int)Math.round(ans/(double)list.size());
-        ans = (int)Math.round(ans/(double)(arr.length - perc*2));
-        System.out.println(ans);
+        System.out.println(sb.toString());
     }
 }
