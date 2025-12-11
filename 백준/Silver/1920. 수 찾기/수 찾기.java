f3ba95ef49707296fd595974d246d0eac7291ce7
chohj2614 <101245494+chohj2614@users.noreply.github.com>
2025-12-11 14:09:01 +0900

    [Silver IV] Title: 수 찾기, Time: 1072 ms, Memory: 51572 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,45 +1,35 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
 public class Main {
     public static void main(String[] args) throws IOException {
 
-        //시간 초과
+        // 3) 이진 탐색
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         String[] inputs = br.readLine().split(" ");
-//        int[] nums = new int[n];
-        Set<Integer> nSet = new HashSet<>();
+        int[] nums = new int[n];
         for(int i = 0; i < n ; i++){
-//            nums[i] = Integer.parseInt(inputs[i]);
-            nSet.add(Integer.parseInt(inputs[i]));
+            nums[i] = Integer.parseInt(inputs[i]);
         }
+        Arrays.sort(nums);
         int m = Integer.parseInt(br.readLine());
         int[] targets = new int[m];
         inputs = br.readLine().split(" ");
         for(int i= 0; i < m; i++){
             targets[i] = Integer.parseInt(inputs[i]);
         }
+
         for(int i = 0; i < m; i++){
-            if(nSet.contains(targets[i])){
-                System.out.println("1");
-            } else{
+            if(Arrays.binarySearch(nums, targets[i]) < 0){
                 System.out.println("0");
+            } else {
+                System.out.println("1");
             }
-//            boolean found = false;
-//            for(int j = 0; j<n; j++){
-//                if(targets[i] == nums[j]){
-//                    System.out.println("1");
-//                    found = true;
-//                    break;
-//                }
-//            }
-//            if(!found){
-//                System.out.println("0");
-//            }
         }
     }
 }
