c839fdfe0e630aedd4f422736fb2a3c4ca25e5c5
chohj2614 <101245494+chohj2614@users.noreply.github.com>
2025-12-11 15:11:01 +0900

    [Gold IV] Title: DSLR, Time: 2984 ms, Memory: 304072 KB -BaekjoonHub

--- 백준/Gold/9019. DSLR/DSLR.java
+++ 백준/Gold/9019. DSLR/DSLR.java
@@ -8,83 +8,73 @@ import java.util.Queue;
 public class Main {
     static int MAX = 10000;
     static int[] dist = new int[MAX + 1];
-    static StringBuilder[] hist = new StringBuilder[MAX + 1];
+    static int[] parent = new int[MAX];
+    static char[] how = new char[MAX];
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
-        for(int t = 0; t < T; t++){
+        for (int t = 0; t < T; t++) {
             String[] inputs = br.readLine().split(" ");
             int start = Integer.parseInt(inputs[0]);
             int target = Integer.parseInt(inputs[1]);
-            reset();
+
             BFS(start, target);
-            System.out.println(hist[target]);
+            System.out.println(recon(start,target));
         }
     }
 
-    public static void reset(){
-        Arrays.fill(dist, -1);
-        Arrays.setAll(hist, i -> new StringBuilder());
-    }
-
     public static void BFS(int start, int target){
+        Arrays.fill(dist, -1);
         Queue<Integer> queue = new LinkedList<>();
         queue.add(start);
         dist[start] = 0;
 
-        while (!queue.isEmpty()){
+        while (!queue.isEmpty()) {
             int cur = queue.poll();
-            if(cur == target){
+            if (cur == target) {
                 return;
             }
-            for(int i = 0; i < 4; i++){
-                int next = 0;
-                String s = "";
-                switch (i){
-                    case 0 :
-                        s = "D";
-                        next = D(cur);
-                        break;
-                    case 1 :
-                        s = "S";
-                        next = S(cur);
-                        break;
-                    case 2 :
-                        s = "L";
-                        next = L(cur);
-                        break;
-                    case 3 :
-                        s = "R";
-                        next = R(cur);
-                        break;
-                }
-                if(dist[next] == -1){
-                    hist[next] = new StringBuilder(hist[cur]).append(s);
-                    dist[next] = dist[cur] + 1;
-                    queue.add(next);
-                }
+            int d = (cur * 2) % 10000;
+            int s = (cur == 0) ? 9999 : cur - 1;
+            int l = (cur % 1000) * 10 + cur / 1000;
+            int r = (cur % 10) * 1000 + cur / 10;
+
+            if (dist[d] == -1) {
+                dist[d] = dist[cur] + 1;
+                parent[d] = cur;
+                how[d] = 'D';
+                queue.add(d);
+            }
+            if (dist[s] == -1) {
+                dist[s] = dist[cur] + 1;
+                parent[s] = cur;
+                how[s] = 'S';
+                queue.add(s);
+            }
+            if (dist[l] == -1) {
+                dist[l] = dist[cur] + 1;
+                parent[l] = cur;
+                how[l] = 'L';
+                queue.add(l);
+            }
+            if (dist[r] == -1) {
+                dist[r] = dist[cur] + 1;
+                parent[r] = cur;
+                how[r] = 'R';
+                queue.add(r);
             }
         }
     }
 
-    public static int D(int n){
-        return (2*n) % 10000;
-    }
-
-    public static int S(int n){
-        if(n == 0 ){
-            return 9999;
+    static String recon(int start, int target){
+        StringBuilder sb = new StringBuilder();
+        while(target != start){
+            sb.append(how[target]);
+            target= parent[target];
         }
-        return  n - 1;
-    }
-
-    public static int L(int n){
-        return (n % 1000)*10 + n/1000;
-    }
-
-    public static int R(int n){
-        return (n % 10) * 1000 + n / 10;
+        sb.reverse();
+        return sb.toString();
     }
 
 }
