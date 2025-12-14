64ae47f8df84e4274a60d3c2bacaa6c9c478a41b
chohj2614 <101245494+chohj2614@users.noreply.github.com>
2025-12-15 00:09:08 +0900

    초기화

--- src/Main.java
+++ src/Main.java
@@ -5,28 +5,7 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-<<<<<<< Updated upstream
-        String[] inputs = br.readLine().split(" ");
-        int N = Integer.parseInt(inputs[0]);
-        int K = Integer.parseInt(inputs[1]);
-        int[] options = new int[N];
-        for(int i = 0; i < N; i++){
-            options[i] = Integer.parseInt(br.readLine());
-        }
-
-        int count = 0;
-        int o_ind = N-1;
-        while(K != 0){
-            int cur = K / options[o_ind];
-            K -= options[o_ind] * cur;
-            o_ind--;
-            count+= cur;
-        }
-
-        System.out.println(count);
-=======
 
         System.out.println();
->>>>>>> Stashed changes
     }
 }
