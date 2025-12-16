4a6ec3fdd98f13e0f4ca4d79bc0a8f1c894ac160
chohj2614 <hyunjoon0614@naver.com>
2025-12-16 17:45:06 +0900

    커밋

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//        https://www.acmicpc.net/problem/1931
+
         System.out.println();
     }
 }
