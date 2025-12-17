fe9650238f730becb390b63162be6acb81937e5d
chohj2614 <101245494+chohj2614@users.noreply.github.com>
2025-12-17 17:40:10 +0900

    [Silver II] Title: 좌표 압축, Time: 2208 ms, Memory: 359376 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.java
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.lang.reflect.Array;
 import java.util.*;
 
 public class Main {
@@ -10,19 +11,23 @@ public class Main {
         int N = Integer.parseInt(br.readLine());
         int[] nums = new int[N];
         String[] inputs = br.readLine().split(" ");
-        Set<Integer> set = new TreeSet<>();
+//        Set<Integer> set = new TreeSet<>(); // 중복 제거는 map에 넣을 때
         for(int n = 0 ; n < N; n++){
             int cur = Integer.parseInt(inputs[n]);
             nums[n] = cur;
-            set.add(cur);
+//            set.add(cur);
         }
+        int[] sorted = Arrays.copyOf(nums,nums.length);
+        Arrays.sort(sorted);
         Map<Integer,Integer> map = new HashMap<>();
         int idx = 0;
-        for(int x : set){
-            map.put(x,idx);
-            idx++;
+        for(int x : sorted){
+            if(!map.containsKey(x)){
+                map.put(x,idx);
+                idx++;
+            }
         }
-
+        
         for(int x: nums){
             sb.append(map.get(x) + " ");
         }
