73b581190adb77c3569b36036ea1559ecb89b49f
chohj2614 <101245494+chohj2614@users.noreply.github.com>
2025-12-21 22:32:16 +0900

    초기화

--- src/Main.java
+++ src/Main.java
@@ -1,65 +1,103 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.LinkedList;
 
 public class Main {
+    static int[][] tetromino1 = {{0,0},{0,1},{0,2},{0,3}}; // 가로 4개
+    static int[][] tetromino2 = {{0,0},{0,1},{1,0},{1,1}}; // 정사각형
+    static int[][] tetromino3 = {{0,0},{1,0},{2,0},{2,1}}; // L 모양
+    static int[][] tetromino4 = {{0,0},{1,0},{1,1},{2,1}}; //
+    static int[][] tetromino5 = {{0,0},{0,1},{0,2},{1,1}}; // ㅗ 뒤집은 모양
+    static int[][][] shapes = {tetromino1,tetromino2,tetromino3,tetromino4,tetromino5};
+    static int[][] map;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
-        StringBuilder sb = new StringBuilder();
-        testLoop:
-        for (int t = 0; t < T; t++) {
-            String operation = br.readLine();
-            int N = Integer.parseInt(br.readLine());
-            String[] arr = br.readLine().replaceAll("[\\[\\]]", "").split(",\\s*");
-            Deque<Integer> deque1 = new ArrayDeque<>();
-            Deque<Integer> deque2 = new ArrayDeque<>();
-            for(int i = 0; i < N; i++){
-                deque1.offerFirst(Integer.parseInt(arr[i]));
-                deque2.offerLast(Integer.parseInt(arr[i]));
-            }
-//            System.out.println(deque1);
-//            System.out.println(deque2);
+        String[] inputs = br.readLine().split(" ");
+        int N = Integer.parseInt(inputs[0]);
+        int M = Integer.parseInt(inputs[1]);
+        map = new int[N][M];
 
-//            List<Integer> result = new ArrayList<>();
-            boolean first = true;
-            for(char c : operation.toCharArray()){
-                if(c == 'R'){
-                    first = !first;
-                }else if(c == 'D'){
-                    if(deque1.isEmpty()){
-                        sb.append("error\n");
-                        continue testLoop;
-                    }
-                    if(first){ // 처음 상태
-                        deque1.pollFirst();
-                        deque2.pollLast();
-                    } else { // 뒤집은 상태
-                        deque1.pollLast();
-                        deque2.pollFirst();
-                    }
-                }
+        for(int i = 0; i < N; i++){
+            inputs = br.readLine().split(" ");
+            for (int j = 0; j < M; j++) {
+                map[i][j] = Integer.parseInt(inputs[j]);
             }
-            sb.append("[");
-            if(first){
-                for(int x : deque1){
-                    System.out.println(x);
-                    sb.append(x+ ", ");
-                }
-            } else {
-                for(int x : deque2){
-                    System.out.println(x);
-                    sb.append(x+ ", ");
-                }
-            }
+        }
+        LinkedList<int[][]> collections = new LinkedList<>();
+        for(int[][] shape : shapes){
+            shape = normalize(shape);
+            collections.add(shape);
+            collections.add(normalize(reflect(shape)));
 
-            sb.deleteCharAt(sb.length()-1);
-            sb.deleteCharAt(sb.length()-2);
-            sb.append("]\n");
-            break;
+            int[][] shape90 = normalize(rotate(shape));
+            collections.add(shape90); // 90
+            collections.add(normalize(reflect(shape90)));
+
+            int[][] shape180 = normalize(rotate(shape90));
+            collections.add(shape180); // 180
+            collections.add(normalize(reflect(shape180)));
+
+            int[][] shape270 = normalize(rotate(shape180));
+            collections.add(shape270); // 270
+            collections.add(normalize(reflect(shape270)));
         }
 
-        System.out.println(sb.toString());
+        System.out.println(findMax(N, M, collections));
     }
+    static int findMax(int Y, int X, LinkedList<int[][]> collections){
+        int max = Integer.MIN_VALUE;
+
+        for(int y = 0; y < Y; y++){
+            for(int x = 0 ; x < X; x++){
+                tetrominoLoop:
+                for(int[][] shape : collections){
+                    int cur = 0;
+                    for (int[] step : shape){
+                        int dx = x + step[1];
+                        int dy = y + step[0];
+                        if(dx < X && dx >= 0 && dy < Y && dy >= 0) cur += map[dy][dx];
+                        else continue tetrominoLoop; // 다음 테트로미노 시도
+                    }
+                    max = Math.max(cur, max);
+                }
+            }
+        }
+        return max;
+    }
+    static int[][] normalize(int[][] shape){
+        int minY = Integer.MAX_VALUE;
+        int minX = Integer.MAX_VALUE;
+        for(int[] x : shape){
+            minY = Math.min(minY,x[0]);
+            minX = Math.min(minX,x[1]);
+        }
+
+        int[][] noramlized = new int[shape.length][2];
+        for (int i = 0; i < shape.length; i++) {
+            noramlized[i][0] = shape[i][0] - minY;
+            noramlized[i][1] = shape[i][1] - minX;
+        }
+        return noramlized;
+    }
+    static int[][] reflect(int[][] shape){
+        int[][] reflected = new int[shape.length][2];
+        for (int i = 0; i < shape.length; i++) {
+            reflected[i][0] = shape[i][0];
+            reflected[i][1] = -shape[i][1];
+        }
+        return reflected;
+    }
+
+    static int[][] rotate(int[][] shape){
+        int[][] rotated = new int[shape.length][2];
+        for (int i = 0; i < shape.length; i++) {
+            rotated[i][0] = shape[i][1];
+            rotated[i][1] = -shape[i][0];
+        }
+        return rotated;
+    }
+
+
 }
