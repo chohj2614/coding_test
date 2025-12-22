5a02fc672a1b80fe25ee1a162163a08d7912ae1d
chohj2614 <101245494+chohj2614@users.noreply.github.com>
2025-12-22 09:48:22 +0900

    [Silver I] Title: 절댓값 힙, Time: 456 ms, Memory: 27400 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.PipedReader;
 import java.util.Comparator;
 import java.util.PriorityQueue;
 
@@ -9,21 +8,19 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        // 1. 람다 형식 --> 2 보다 성능이 좋음
-        PriorityQueue<Integer> pq =
-            new PriorityQueue<>((a,b) -> {
-                int absA = Math.abs(a);
-                int absB = Math.abs(b);
-                if(absA!= absB) return absA - absB; // 다를 경우 절대값 비교
-                return a-b; //같은 절대값 -> 자연수로 비교
-            });
-        // 2. Comparator.comparingInt 형식
-//        PriorityQueue<Integer> pq =
-//                new PriorityQueue<>(
-//                        Comparator.comparing((Integer x) -> Math.abs(x))
-//                                .thenComparingInt(x->x)
-//                );
-        for(int i = 0; i < N; i++){
+        PriorityQueue<Integer> pq = new PriorityQueue<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                int temp1 = Math.abs(o1);
+                int temp2 = Math.abs(o2);
+                if(temp1 == temp2){
+                    return o1 - o2;
+                }
+                return temp1 - temp2;
+            }
+        });
+
+        for (int i = 0; i < N; i++) {
             int cur = Integer.parseInt(br.readLine());
             if(cur == 0){
                 if(pq.isEmpty()) System.out.println(0);
@@ -32,5 +29,6 @@ public class Main {
                 pq.offer(cur);
             }
         }
+//        System.out.println();
     }
 }
